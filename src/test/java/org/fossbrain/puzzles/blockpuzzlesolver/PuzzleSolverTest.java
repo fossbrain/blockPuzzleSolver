2f7250bb8af39fc581c7d1518b135aadda64062c
Andreas Pieber <anpieber@gmail.com>
2012-03-31 16:26:27 +0200

    Added additional test case
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- src/test/java/org/fossbrain/puzzles/blockpuzzlesolver/PuzzleSolverTest.java
+++ src/test/java/org/fossbrain/puzzles/blockpuzzlesolver/PuzzleSolverTest.java
@@ -59,6 +59,40 @@ public class PuzzleSolverTest {
     }
 
     @Test
+    public void lvlPack1_lvl263_solver() {
+        String result = new StringBuilder()
+            .append("p p p p o o g g \n")
+            .append("v v v d o o g l \n")
+            .append("v y d d d g g l \n")
+            .append("v y r r r r r l \n")
+            .toString();
+
+        Block red = new Block('r', new boolean[][]{ { true, true, true, true, true } });
+        Block green =
+            new Block('g', new boolean[][]{ { false, true, true }, { false, true, false }, { true, true, false } });
+        Block violett =
+            new Block('v', new boolean[][]{ { true, true, true }, { true, false, false }, { true, false, false } });
+        Block yellow = new Block('y', new boolean[][]{ { true }, { true } });
+        Block lightBlue = new Block('l', new boolean[][]{ { true }, { true }, { true } });
+        Block orange = new Block('o', new boolean[][]{ { true, true }, { true, true } });
+        Block darkBlue = new Block('d', new boolean[][]{ { false, true, false }, { true, true, true } });
+        Block pink = new Block('p', new boolean[][]{ { true, true, true, true } });
+
+        Game baseBoard = new Game().withLenght(8).withHeight(4);
+        baseBoard.withBlock(red);
+        baseBoard.withBlock(violett);
+        baseBoard.withBlock(lightBlue);
+        baseBoard.withBlock(yellow);
+        baseBoard.withBlock(green);
+        baseBoard.withBlock(orange);
+        baseBoard.withBlock(darkBlue);
+        baseBoard.withBlock(pink);
+        baseBoard.solve();
+
+        assertEquals(result, baseBoard.printBoard());
+    }
+
+    @Test
     public void lvlPack2_lvl1_solver() {
         String result = new StringBuilder()
             .append("y y y g g \n")
