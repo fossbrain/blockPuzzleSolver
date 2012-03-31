5b50d2c95b700fc6c047e739e5eeea7f7e66ece3
Andreas Pieber <anpieber@gmail.com>
2012-03-31 09:38:11 +0200

    Archetype layout
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

---
+++ src/main/java/org/fossbrain/puzzles/blockpuzzlesolver/BlockPuzzleSolver.java
@@ -0,0 +1,12 @@
+package org.fossbrain.puzzles.blockpuzzlesolver;
+
+/**
+ * Entry point for the puzzle solver
+ */
+public class BlockPuzzleSolver {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/fossbrain/puzzles/blockpuzzlesolver/PuzzleSolver.java
@@ -0,0 +1,8 @@
+package org.fossbrain.puzzles.blockpuzzlesolver;
+
+/**
+ * Class for solving the puzzle
+ */
+public class PuzzleSolver {
+
+}
