a537ada22a7538b4f7ba48ba1456183560f64368
Andreas Pieber <anpieber@gmail.com>
2012-03-31 14:08:29 +0200

    Added initial test layout

---
+++ src/test/java/org/fossbrain/puzzles/blockpuzzlesolver/PuzzleSolverTest.java
@@ -0,0 +1,14 @@
+package org.fossbrain.puzzles.blockpuzzlesolver;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PuzzleSolverTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
